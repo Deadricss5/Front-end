b3eccec8a0eea4c8c65a641c68a043f500a272fe
Sergey Suprun <serg.suprun@gmail.com>
2020-07-30 11:16:22 +0300

    fix images
    fix images

--- the-rosa/main.js
+++ the-rosa/main.js
@@ -25,19 +25,19 @@ sr.reveal(".animate-right", {
   origin: "right",
   duration: 1000,
   distance: "25rem",
-  delay: 600,
+  delay: 300,
 });
 
 sr.reveal(".animate-top", {
   origin: "top",
   duration: 1000,
   distance: "25rem",
-  delay: 600,
+  delay: 300,
 });
 
 sr.reveal(".animate-bottom", {
   origin: "bottom",
   duration: 1000,
   distance: "25rem",
-  delay: 600,
+  delay: 300,
 });
